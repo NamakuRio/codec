5264020302220a2d8c8c6325736435803b25a403
Rio Prastiawan <akunviprio@gmail.com>
2020-01-25 00:14:45 +0700

    update file AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Force https if app debug false
+        if (config('app.debug') != true) {
+            URL::forceScheme('https');
+        }
+
+        // Default string length 191 character
+        Schema::defaultStringLength(191);
+
+        // Blade Directive Asset
+        Blade::directive('asset', function ($asset) {
+            return "<?= asset($asset) ?>";
+        });
+
+        // Blade Directive Trans
+        Blade::directive('trans', function ($trans) {
+            return "<?= trans($trans) ?>";
+        });
+
+        // Blade Directive Route
+        Blade::directive('route', function ($route) {
+            return "<?= route($route) ?>";
+        });
     }
 }
